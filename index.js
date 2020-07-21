a4c044c4d340de5e7c07ec7574d8d66d12c2b526
izaanjahangir <izaanjahangir2@gmail.com>
2020-07-21 20:53:04 +0500

    setup global error handler

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
 
+const globalHelpers = require("./utils/globalHelpers");
 const keys = require("./config/keys");
+
+// Global error handler
+app.use((err, req, res, next) => {
+  const error = globalHelpers.handleMongooseError(err.message);
+
+  if (
+    process.env.NODE_ENV === "production" &&
+    typeof error.message === "string" &&
+    error.message.startsWith("request to http")
+  ) {
+    return res
+      .status(500)
+      .json({ message: "Internal server error", status: "failed" });
+  }
+
+  res.status(err.status || 400).json({ ...error, status: "failed" });
+});
 
 app.listen(keys.PORT, () =>
   console.log("Server is running on PORT ", keys.PORT)
