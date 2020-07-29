c4cb56f7a318a99449def7258d14beb3efdbc2e6
izaanjahangir <izaanjahangir2@gmail.com>
2020-07-29 20:51:21 +0500

    [client] added basic ChatHeader component

--- client/src/screens/Home/components/ChatHeader/index.js
+++ client/src/screens/Home/components/ChatHeader/index.js
@@ -1,10 +1,20 @@
 import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+import Avatar from "../../../../components/Avatar";
 import useStyles from "./style";
 
 function ChatHeader() {
   const classes = useStyles();
 
-  return <div className={classes.headerContainer}></div>;
+  return (
+    <div className={classes.container}>
+      <Avatar />
+      <Typography className={classes.username} variant="subtitle1">
+        Izaan jahangir
+      </Typography>
+    </div>
+  );
 }
 
 export default ChatHeader;
--- client/src/screens/Home/components/ChatHeader/style.js
+++ client/src/screens/Home/components/ChatHeader/style.js
@@ -1,5 +1,16 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  container: {
+    minHeight: "70px",
+    display: "flex",
+    alignItems: "center",
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+  },
+  username: {
+    paddingLeft: theme.spacing(2),
+  },
+}));
 
 export default useStyles;
